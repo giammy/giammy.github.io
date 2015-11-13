21b729ddfa3ba26d8f69f75e85eec7381bb5407d
giammy <giangiammy@gmail.com>
2015-11-13 11:26:33 +0100

    Initial commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+
+
+http://alexcican.com/post/guide-hosting-website-dropbox-github/
+https://pages.github.com/
+
+
+Head over to GitHub and create a new repository named giammy.github.io
+
+
+git clone https://github.com/giammy/giammy.github.io
+git add --all
+git commit -m "Initial commit"
+git push -u origin master
---
+++ make.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function makeBase {
+    cat header.html.tmpl > $1
+    cat $1.tmpl >> $1
+    cat footer.html.tmpl >> $1
+}
+
+for i in index hsk art riflessioni nsa cloudusb misc about bci ; do 
+    makeBase $i.html  
+done
+
+( pushd . ; cd .. ; rsync -av --delete giammy ~/Dropbox/Public/ ;  popd )
+
+echo https://dl.dropboxusercontent.com/u/2092071/giammy/index.html
+
+#function makeDirs {
+#    cat headerdirs.html.tmpl > $1
+#    cat $1.tmpl >> $1
+#    cat footer.html.tmpl >> $1
+#}
+#for i in xxx ; do 
+#    makeDirs $i/index.html  
+#done
+
+
+# mkdir -p ~/Desktop/tmp
+# zip ~/Desktop/tmp/home.zip * ; for i in abstractart css files fonts hsk img js ; do zip -r ~/Desktop/tmp/$i.zip $i ; done
+
+
