6842cd0487dd3f436c0ab935cfd34828c21aaad8
giammy <giangiammy@gmail.com>
2016-01-19 17:27:44 +0100

    minor clean

--- make.sh
+++ make.sh
@@ -10,10 +10,11 @@ for i in index hsk art riflessioni nsa cloudusb misc about bci gadgets ; do
     makeBase $i.html  
 done
 
-( pushd . ; cd .. ; rsync -av --delete giammy.github.io/* ~/Dropbox/Public/giammy/ ;  popd )
-
-echo https://dl.dropboxusercontent.com/u/2092071/giammy/index.html
-
+#
+#( pushd . ; cd .. ; rsync -av --delete giammy.github.io/* ~/Dropbox/Public/giammy/ ;  popd )
+#
+#echo https://dl.dropboxusercontent.com/u/2092071/giammy/index.html
+#
 #function makeDirs {
 #    cat headerdirs.html.tmpl > $1
 #    cat $1.tmpl >> $1
@@ -22,9 +23,9 @@ echo https://dl.dropboxusercontent.com/u/2092071/giammy/index.html
 #for i in xxx ; do 
 #    makeDirs $i/index.html  
 #done
-
-
+#
+#
 # mkdir -p ~/Desktop/tmp
 # zip ~/Desktop/tmp/home.zip * ; for i in abstractart css files fonts hsk img js ; do zip -r ~/Desktop/tmp/$i.zip $i ; done
-
+#
 
