a18da263297b4b0c40e19b902a5dbc27845993ff
Gianluca Moro <giangiammy@gmail.com>
2018-02-28 16:14:57 +0100

    minor refactoring

--- sea/js/sea.js
+++ sea/js/sea.js
@@ -1,3 +1,20 @@
+/*
+ * SeA - Sintomi e Azioni
+ * Copyright (C) 2018 - Gianluca Moro - giangiammy@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 class SeaData {
 
@@ -54,10 +71,10 @@ class SeaData {
     getProblemaMsg(i) {
 	return this.data['p'+i.toString()];
     }
+
     getAzioneMsg(i) {
 	return this.data['a'+i.toString()];
     }
-
 
     getAll(typ) {
 	var allStr = [];
