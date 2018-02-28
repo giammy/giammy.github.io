512fef34c6225fcd49002e813add04001c435fb9
Gianluca Moro <giangiammy@gmail.com>
2018-02-28 17:58:48 +0100

    separate data in a file for sintomi

--- sea/js/sea.js
+++ sea/js/sea.js
@@ -20,56 +20,7 @@ class SeaData {
 
     constructor (data) {
 
-	this.dataStr = `
-{
-  "versione": "201802281000",
-  "s0": "difficolta' respiratoria", 
-  "s1": "uso muscoli accessori per respirare",
-  "s2": "eloquio limitato",
-  "s3": "rumori respiratori (borbottio - fischi - sibili)",
-  "s4": "frequenza respiratoria <8",
-  "s5": "cianosi",
-  "s6": "ritmo respiratorio alterato",
-  "s7": "dolore al torace",
-  "s8": "dolore allo sterno",
-  "s9": "frequenza respiratoria > 30",
-  "s10":"alterazione dello stato mentale",
-  "s11": "sonnolenza",
-  "s12": "soporoso",
-  "s13": "tosse",
-  "s14": "espettorato",
-
-  "p0": "ictus",
-  "p1": "arresto cardiaco",
-  "p2": "edema polmonare",
-  "p3": "insufficienza respiratoria",
-  "p4": "ostruzione vie aeree superiori",
-  "p5": "asma",
-  "p6": "BPCO (broncopneumopatie croniche ostruttive)",
-  "p7": "Bronchite cronica",
-  "p8": "enfisema",
-  "p9": "Polmonite",
-  "p10":"Edema polmonare",
-  "p11":"Pleurite",
-  "p12":"Pneumotorace",
-  "p13":"Embolia polmonare",
-
-  "a0": "far tossire",
-  "a1": "ABC",
-  "a2": "somministrare ossigeno",
-  "a3": "non far camminare",
-  "a4": "rassicurare",
-  "a5": "posizione seduta/semiseduta",
-  "sep": [ 
-	{ "s": [0, 1, 2, 3, 4, 5, 6, 9, 10, 11, 12 ], "p": [3], "a": [1, 2, 3, 4, 5 ] },
-	{ "s": [3,5,13 ], "p": [4], "a": [0] },
-	{ "s": [0,3,13], "p": [5], "a": [1,2,5] },
-	{ "s": [3,13,14], "p": [6], "a": [1,2,5] },
-
-	{ "s": [1, 2, 3, 4, 5, 6, 7, 8 ], "p": [1], "a": [1, 2, 4, 5 ] }
-       ]
-}`;
-
+	this.dataStr = data;
 	this.data = JSON.parse(this.dataStr);
 	this.internalSintomiAll = this.getAll("s");
 	this.internalProblemiAll = this.getAll("p");
