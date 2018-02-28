5d9b792657f79d0ac828be27fc3965ab4b2e961a
Gianluca Moro <giangiammy@gmail.com>
2018-02-28 15:14:02 +0100

    added sea

---
+++ sea/do-cp2prod.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -r * /Users/giammy/Dropbox/data-rem1/site/giammy.github.io/sea
---
+++ sea/js/sea.js
@@ -0,0 +1,104 @@
+
+class SeaData {
+
+    constructor (data) {
+
+	this.dataStr = `
+{
+  "versione": "201802181000",
+  "s0": "S0", 
+  "s1": "uso muscoli accessori per respirare",
+  "s2": "eloquio limitato",
+  "s3": "rumori respiratori (borbottio - fischi - sibili)",
+  "s4": "frequenza respiratoria <8 o > 30",
+  "s5": "cianosi",
+  "s6": "ritmo respiratorio alterato",
+  "s7": "dolore al torace",
+  "s8": "dolore allo sterno",
+
+  "p0": "ictus",
+  "p1": "arresto cardiaco",
+  "p2": "edema polmonare",
+  "p3": "insufficienza respiratoria",
+
+  "a0": "A0",
+  "a1": "ABC",
+  "a2": "somministrare ossigeno",
+  "a3": "non far camminare",
+  "a4": "rassicurare",
+  "a5": "posizione seduta/semiseduta",
+  "sep": [ 
+	{ "s": [1, 2, 3, 4, 5, 6 ], "p": [3], "a": [1, 2, 3, 4, 5 ] },
+	{ "s": [1, 2, 3, 4, 5, 6, 7, 8 ], "p": [1], "a": [1, 2, 4, 5 ] }
+       ]
+}`;
+
+	this.data = JSON.parse(this.dataStr);
+	this.internalSintomiAll = this.getAll("s");
+	this.internalProblemiAll = this.getAll("p");
+	this.internalAzioniAll = this.getAll("a");
+
+	this.internalSintomiCkecked = this.internalSintomiAll.map(function (x) { return false; });
+	this.internalProblemiCorrenti = [];
+	this.internalAzioniCorrenti = [];
+    }
+
+    get sintomiAll  () { return this.internalSintomiAll;  }
+    get problemiAll () { return this.internalProblemiAll; }
+    get azioniAll   () { return this.internalAzioniAll;   }
+
+    get sintomiChecked   () { return this.internalSintomiCkecked;   }
+    get problemiCorrenti () { return this.internalProblemiCorrenti; }
+    get azioniCorrenti   () { return this.internalAzioniCorrenti;   }
+
+    getProblemaMsg(i) {
+	return this.data['p'+i.toString()];
+    }
+    getAzioneMsg(i) {
+	return this.data['a'+i.toString()];
+    }
+
+
+    getAll(typ) {
+	var allStr = [];
+	for (var i=0;;i++) {
+	    var indexStr = typ + i.toString();
+	    if (indexStr in this.data) {
+		allStr.push(this.data[indexStr]);
+	    } else {
+		break;
+	    }
+	}
+	return allStr;
+    }
+
+    switchSintomo(i) {
+	this.internalSintomiCkecked[i] = this.internalSintomiCkecked[i]?false:true;
+	this.updateProblemiAzioniCorrenti();
+    }
+
+    isSIncluded(desc, i) {
+	return (desc.s.indexOf(i) > -1);
+    }
+
+    updateProblemiAzioniCorrenti() {
+	this.internalProblemiCorrenti = [];
+	this.internalAzioniCorrenti = [];
+	var usep = [];
+	for (var i=0; i<this.internalSintomiCkecked.length; i++) {
+	    if (this.internalSintomiCkecked[i]) {
+		for (var k=0; k<this.data.sep.length; k++) {
+		    if (this.isSIncluded(this.data.sep[k], i)) {
+			usep.push(this.data.sep[k]);
+		    }
+		}
+
+	    }
+	}
+	for (var j=0; j<usep.length; j++) {
+	    this.internalProblemiCorrenti = jQuery.unique(this.internalProblemiCorrenti.concat(usep[j]['p']).sort());
+	    this.internalAzioniCorrenti = jQuery.unique(this.internalAzioniCorrenti.concat(usep[j]['a']).sort());
+	}
+    }
+
+}
